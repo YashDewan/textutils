e494e14b37d2e8c7e4ebed21645627c5a2d92f01
Yash Dewan <yashdewan2580@gmail.com>
2022-05-02 17:47:01 +0530

    build

---
+++ build/precache-manifest.440ff8620e56ae906daf5e2cc1d27c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69317fc7d8e6def312db",
+    "url": "/static/css/main.e74bf87d.chunk.css"
+  },
+  {
+    "revision": "69317fc7d8e6def312db",
+    "url": "/static/js/main.69317fc7.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "79ab0a8aa4df99e00913",
+    "url": "/static/js/2.79ab0a8a.chunk.js"
+  },
+  {
+    "revision": "7ee0e2da555c3fd35366",
+    "url": "/static/js/runtime~main.7ee0e2da.js"
+  },
+  {
+    "revision": "8b0eafbdfb114c8b38ef5934ab3eb739",
+    "url": "/index.html"
+  }
+];
